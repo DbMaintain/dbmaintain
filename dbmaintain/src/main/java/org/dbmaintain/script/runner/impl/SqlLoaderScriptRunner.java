38301daffa16daf0bfbb7ba3ce705845acb7c8ff
Pascal Wittmann <pascal.wittmann@red6-es.de>
2018-07-12 14:10:41 +0200

    Remove unused imports

--- dbmaintain/src/main/java/org/dbmaintain/script/runner/impl/SqlLoaderScriptRunner.java
+++ dbmaintain/src/main/java/org/dbmaintain/script/runner/impl/SqlLoaderScriptRunner.java
@@ -19,7 +19,6 @@ package org.dbmaintain.script.runner.impl;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
--- dbmaintain/src/main/java/org/dbmaintain/util/CollectionUtils.java
+++ dbmaintain/src/main/java/org/dbmaintain/util/CollectionUtils.java
@@ -15,7 +15,6 @@
  */
 package org.dbmaintain.util;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
